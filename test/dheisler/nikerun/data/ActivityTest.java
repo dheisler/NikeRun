b93d6bf2c1ceee1f50b790b6f56ae42f7e48e548
dheisler <debbieheisler@gmail.com>
2018-04-27 16:06:07 -0700

    finished activity for now

--- test/dheisler/nikerun/data/ActivityTest.java
+++ test/dheisler/nikerun/data/ActivityTest.java
@@ -10,6 +10,8 @@ class ActivityTest
     private static final String activityId = "aShortActivityId";
     private static final String startTime = "2017-04-30T18:01:50Z";
     private static final String endTime = "2017-04-30T18:50:31.166Z";
+    private static final double distance5 = 5.011560229879804;
+    private static final double distance0 = 0.8650855;
 
     @BeforeEach
     public void setup()
@@ -21,13 +23,11 @@ class ActivityTest
     public void addActivityTypeOther()
     {
         Activity activity = getActivity(activityId, Activity.OTHER);
-        activity.setStartTime(startTime);
-        activity.setEndTime(endTime);
+        addStartEndTimeToActivity(activity);
         assertEquals(Activity.OTHER, activity.getActivityType());
         assertEquals(startTime, activity.getStartTime());
         assertEquals(endTime, activity.getEndTime());
     }
-
 
     @Test
     public void addActivityTypeTraining()
@@ -41,12 +41,22 @@ class ActivityTest
     {
         Activity activity = getActivity(activityId, Activity.RUN);
         assertEquals(Activity.RUN, activity.getActivityType());
+        addStartEndTimeToActivity(activity);
+        activity.setDistance(distance0);
+        assertFalse(activity.ranMoreThankOneK());
+        activity.setDistance(distance5);
+        assertTrue(activity.ranMoreThankOneK());
     }
 
-
-    private Activity getActivity(String activityId, int other)
+    private Activity getActivity(String activityId, int type)
     {
-        return new Activity(activityId, other);
+        return new Activity(activityId, type);
+    }
+
+    private void addStartEndTimeToActivity(Activity activity)
+    {
+        activity.setStartTime(startTime);
+        activity.setEndTime(endTime);
     }
 
 }
